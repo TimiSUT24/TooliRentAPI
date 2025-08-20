c359110702cd51098e3c3ad92b858e97217b3021
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-08-20 14:12:36 +0200

    added jwt Authentication in program..cs

--- TooliRentAPI/Program.cs
+++ TooliRentAPI/Program.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -21,7 +22,10 @@ namespace TooliRentAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            //AddScoped Services
             builder.Services.AddScoped<IJwt, JwtService>();
+
             //Get ConnectionString for db
             builder.Services.AddDbContext<TooliRentDBContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -39,6 +43,29 @@ namespace TooliRentAPI
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<TooliRentDBContext>()
                 .AddDefaultTokenProviders();
+
+            //Jwt Authentication 
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["JWT:Issuer"],
+                        ValidAudience = builder.Configuration["JWT:Audience"],
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+                    };
+                });
+
+            builder.Services.AddAuthorization(); 
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
