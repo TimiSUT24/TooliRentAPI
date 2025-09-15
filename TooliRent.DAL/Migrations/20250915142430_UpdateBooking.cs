ab878080efc7af92b4f6ea7487db630d1aaeb4ee
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-09-15 16:25:07 +0200

    updated booking model

---
+++ TooliRent.DAL/Migrations/20250915142430_UpdateBooking.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TooliRent.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLate",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latefee",
+                table: "Bookings",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Latefee",
+                table: "Bookings");
+        }
+    }
+}
